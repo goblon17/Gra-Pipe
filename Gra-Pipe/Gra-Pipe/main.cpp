97a035623434ff101f0e5afb83eef2dc8c1821e7
Szymon Kaczmarek <szymon.kaczmarek111@gmail.com>
2022-05-21 10:56:01 +0200

    added GLEW, GLFW and GLM

--- Gra-Pipe/Gra-Pipe/main.cpp
+++ Gra-Pipe/Gra-Pipe/main.cpp
@@ -1,9 +1,43 @@
 #include <cstdio>
+#include <cstdlib>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 //haha chuj xd
 
 int main() {
-	printf("Hello World!\n");
-	printf("Hello from the other side\n");
+	GLFWwindow* window;
+
+	if (!glfwInit()) {
+		fprintf(stderr, "Nie udalo sie zainicjowac GLFW.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	window = glfwCreateWindow(500, 500, "PipeGame", NULL, NULL);
+
+	if (!window) {
+		fprintf(stderr, "Nie udalo sie utworzyc okna.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Nie udalo sie zainicjowac GLEW.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	while (!glfwWindowShouldClose(window)) {
+		int a = 0;
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
+
 	return 0;
 }
