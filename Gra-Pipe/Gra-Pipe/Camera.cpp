6f51ff9ab4dfab9e0325d784ec1ab88696a074a4
Szymon Kaczmarek <szymon.kaczmarek111@gmail.com>
2022-05-22 19:10:04 +0200

    Created Camera class

---
+++ Gra-Pipe/Gra-Pipe/Camera.cpp
@@ -0,0 +1,56 @@
+#include "Camera.h"
+
+Camera::Camera(glm::vec3 pos, glm::vec3 target, glm::vec3 up, float FOV, float aspectRatio, float closePlane, float farPlane) {
+	this->pos = pos;
+	this->target = target;
+	this->up = up;
+	this->FOV = FOV * PI / 180.0f;
+	this->aspectRatio = aspectRatio;
+	this->closePlane = closePlane;
+	this->farPlane = farPlane;
+	this->Vmat = this->calculateVmat();
+	this->Pmat = this->calculatePmat();
+
+	this->radius = glm::distance(pos, target);
+	this->alfa = this->calcAlfa();
+	this->beta = this->calcBeta();
+	this->resolution = 180;
+}
+
+glm::mat4 Camera::calculateVmat() {
+	return glm::lookAt(
+		this->pos,
+		this->target,
+		this->up
+	);
+}
+
+glm::mat4 Camera::calculatePmat() {
+	return glm::perspective(this->FOV, this->aspectRatio, this->closePlane, this->farPlane);
+}
+
+glm::vec3 Camera::calcPos() {
+	float x = this->target.x + this->radius * glm::cos(this->alfa) * glm::cos(this->beta);
+	float y = this->target.y + this->radius * glm::sin(this->beta);
+	float z = this->target.z + this->radius * glm::sin(this->alfa) * glm::cos(this->beta);
+	return glm::vec3(x, y, z);
+}
+
+float Camera::calcAlfa() {
+	float sinB = (this->pos.y - this->target.y) / this->radius;
+	float cosB = glm::sqrt(1 - sinB * sinB);
+	float sinA = (this->pos.z - this->target.z) / (this->radius * cosB);
+	return glm::asin(sinA);
+}
+
+float Camera::calcBeta() {
+	float sinB = (this->pos.y - this->target.y) / this->radius;
+	return glm::asin(sinB);
+}
+
+void Camera::changePos(double dX, double dY) {
+	this->alfa += dX * PI / 180.0f;
+	this->beta += dX * PI / 180.0f;
+	this->pos = calcPos();
+	this->Vmat = this->calculateVmat();
+}
---
+++ Gra-Pipe/Gra-Pipe/Camera.h
@@ -0,0 +1,43 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#define GLM_FORCE_RADIANS
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include "Utility.h"
+
+class Camera
+{
+private:
+	glm::vec3 pos;
+	glm::vec3 target;
+	glm::vec3 up;
+
+	float FOV;
+	float closePlane;
+	float farPlane;
+
+	float radius;
+	float alfa;
+	float beta;
+	float resolution;
+
+	glm::vec3 calcPos();
+	float calcAlfa();
+	float calcBeta();
+
+public:
+	glm::mat4 Vmat;
+	glm::mat4 Pmat;
+	float aspectRatio;
+
+	glm::mat4 calculateVmat();
+	glm::mat4 calculatePmat();
+
+	Camera(glm::vec3 pos, glm::vec3 target, glm::vec3 up, float FOV, float aspectRatio, float closePlane, float fatPlane);
+
+	void changePos(double dX, double dY);
+};
+
+#endif
