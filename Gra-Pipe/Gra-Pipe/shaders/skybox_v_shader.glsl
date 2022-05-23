26aa645d2a6212bebe837a29062f32d5ab7d05b2
Szymon Kaczmarek <szymon.kaczmarek111@gmail.com>
2022-05-23 13:51:09 +0200

    Made changes to properly compute skybox

--- Gra-Pipe/Gra-Pipe/shaders/skybox_v_shader.glsl
+++ Gra-Pipe/Gra-Pipe/shaders/skybox_v_shader.glsl
@@ -4,12 +4,15 @@ uniform mat4 P;
 uniform mat4 V;
 uniform mat4 M;
 
-in vec4 Vertex;
-in vec3 texCoord;
+uniform float Size;
+
+in vec3 Vertex;
 
 out vec3 iTexCoord;
 
 void main(void) {
-	iTexCoord = texCoord;
-	gl_Position = P * V * M * Vertex;
+	iTexCoord = Vertex;
+	mat4 nV = V;
+	nV[3].xyz = vec3(0, 0, 0);
+	gl_Position = P * nV * M * vec4(Vertex * Size, 1);
 }
