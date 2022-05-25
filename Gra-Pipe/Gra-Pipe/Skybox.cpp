e88e230b2f5e290926806ca3c1d010b7a16d411a
Szymon Kaczmarek <szymon.kaczmarek111@gmail.com>
2022-05-26 01:25:10 +0200

    Modified

--- Gra-Pipe/Gra-Pipe/Skybox.cpp
+++ Gra-Pipe/Gra-Pipe/Skybox.cpp
@@ -12,20 +12,20 @@ Skybox::~Skybox() {
 void Skybox::Draw(Camera* camera) {
 	this->skyboxShader->use();
 
-	glUniformMatrix4fv(skyboxShader->u("P"), 1, false, glm::value_ptr(camera->Pmat));
-	glUniformMatrix4fv(skyboxShader->u("V"), 1, false, glm::value_ptr(camera->Vmat));
-	glUniformMatrix4fv(skyboxShader->u("M"), 1, false, glm::value_ptr(this->M));
+	glUniformMatrix4fv(this->skyboxShader->u("P"), 1, false, glm::value_ptr(camera->Pmat));
+	glUniformMatrix4fv(this->skyboxShader->u("V"), 1, false, glm::value_ptr(camera->Vmat));
+	glUniformMatrix4fv(this->skyboxShader->u("M"), 1, false, glm::value_ptr(this->M));
 
-	glUniform1i(skyboxShader->u("skybox"), 0);
+	glUniform1i(this->skyboxShader->u("skybox"), 0);
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_CUBE_MAP, this->tex);
 
-	glUniform1f(skyboxShader->u("Size"), this->size);
+	glUniform1f(this->skyboxShader->u("Size"), this->size);
 
-	glEnableVertexAttribArray(skyboxShader->a("Vertex"));
-	glVertexAttribPointer(skyboxShader->a("Vertex"), 3, GL_FLOAT, false, 0, this->verts);
+	glEnableVertexAttribArray(this->skyboxShader->a("Vertex"));
+	glVertexAttribPointer(this->skyboxShader->a("Vertex"), 3, GL_FLOAT, false, 0, this->verts);
 
 	glDrawArrays(GL_TRIANGLES, 0, vertCount);
 
-	glDisableVertexAttribArray(skyboxShader->a("Vertex"));
+	glDisableVertexAttribArray(this->skyboxShader->a("Vertex"));
 }
